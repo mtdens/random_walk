623b5110d0f6ad39d05b6a61989d91c016f3674f
Matthew <mdenslinger@gmail.com>
2016-07-18 13:19:51 -0400

    Added colors and radius

--- walk.js
+++ walk.js
@@ -2,9 +2,21 @@
 var can = document.getElementById("walk");
 var con = can.getContext("2d");
 
+var particle_colors = ["red", "blue", "green", "orange", "purple", "black", "yellow", "grey"];
+var particle_list = [];
+
 //Particle
-function Particle(){
-	this.x = can.width/2; //Should be canvas.x/2
-	this.y = can.height/2;	//Should be canvas.y/2
+function Particle() {
+	this.x = can.width/2;
+	this.y = can.height/2;
 	var radius = 4;
+	var color = particle_colors[Math.floor(Math.random()*particle_colors.length-1)];
+	
+	this.get_radius = function() {
+		return radius;
+	};
+	
+	this.get_color = function(){
+		return color;
+	};
 }
